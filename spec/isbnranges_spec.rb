e4e77452cc0dca03736040e163ec1c40eb002eec
takatoh <takatoh.m@gmail.com>
2024-08-13 13:52:21 +0900

    First commit.

---
+++ spec/isbnranges_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ISBNRanges do
+  it "has a version number" do
+    expect(ISBNRanges::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
