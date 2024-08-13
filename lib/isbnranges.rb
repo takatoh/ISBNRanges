8be1535e6e2a99ffab96801435ff65b60ffd0086
takatoh <takatoh.m@gmail.com>
2024-08-13 14:51:10 +0900

    lib/isbnranges.rb(load_ranges): New.

--- lib/isbnranges.rb
+++ lib/isbnranges.rb
@@ -3,6 +3,18 @@
 require_relative "isbnranges/version"
 
 module ISBNRanges
-  class Error < StandardError; end
+#  class Error < StandardError; end
   # Your code goes here...
+
+  def load_ranges(range_file)
+    ranges = {}
+    File.open(range_file, "r") do |f|
+      f.each_line do |line|
+        next if line.start_with?("#")
+        g, r = line.chomp.split(":")
+        ranges[g] = r.split(",") unless r.nil?
+      end
+    end
+    ranges
+  end
 end
