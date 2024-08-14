3246e138323bf4d3e615faa16d9b092a700eb2fa
takatoh <takatoh.m@gmail.com>
2024-08-14 12:08:48 +0900

    lib/isbnranges.rb: Define constant: REGISTRATION_GROUP_AGENCY

--- lib/isbnranges.rb
+++ lib/isbnranges.rb
@@ -12,18 +12,20 @@ module ISBNRanges
 
   def load_ranges(range_file)
     ranges = {}
+    agencies = {}
     File.open(range_file, "r") do |f|
       f.each_line do |line|
         next if line.start_with?("#")
-        g, r = line.chomp.split(":")
+        g, r, a = line.chomp.split(":")
         ranges[g] = r.split(",") unless r.nil?
+        agencies[g] = a unless r.nil?
       end
     end
-    ranges
+    [ranges, agencies]
   end
 
   data_dir = __dir__ + "/../data"
-  REGISTRATION_GROUP_RANGES = load_ranges("#{data_dir}/#{ISBNRanges::REGISTRATION_GROUP_RANGES_FILE}")
-  REGISTRANT_RANGES = load_ranges("#{data_dir}/#{ISBNRanges::REGISTRANT_RANGES_FILE}")
+  REGISTRATION_GROUP_RANGES, _ = load_ranges("#{data_dir}/#{ISBNRanges::REGISTRATION_GROUP_RANGES_FILE}")
+  REGISTRANT_RANGES, REGISTRATION_GROUP_AGENCY = load_ranges("#{data_dir}/#{ISBNRanges::REGISTRANT_RANGES_FILE}")
   RANGE_DATE = Date.parse(File.read("#{data_dir}/#{ISBNRanges::RANGE_DATE_FILE}"))
 end
