6d6858b2e8daa30b1ffd863b9f21b854916c31ff
takatoh <takatoh.m@gmail.com>
2024-08-14 09:34:38 +0900

    lib/isbnranges/datafiles.rb: New. Define data file names.

---
+++ lib/isbnranges/datafiles.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module ISBNRanges
+  RANGE_MESSAGE_FILE = "RangeMessage.xml"
+  REGISTRATION_GROUP_RANGES_FILE = "registration_group_ranges.txt"
+  REGISTRANT_RANGES_FILE = "registrant_ranges.txt"
+  RANGE_DATE_FILE = "range_date.txt"
+end
